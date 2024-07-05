e19e6166e4da272f717527515f2d7347f0f0a6b9
PangKiYeon <pdrkeamy@gmail.com>
2024-07-05 17:16:52 +0900

    Feat : typography config, stories 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,21 @@
-import type { Config } from "tailwindcss";
-import { merge } from "lodash";
-import tailwindCustomConfig from "./tailwind_custom.config";
-import tailwindElevationConfig from "./tailwind_elevation.config";
+import type { Config } from 'tailwindcss';
+import { merge } from 'lodash';
+import tailwindCustomConfig from './tailwind_custom.config';
+import tailwindElevationConfig from './tailwind_elevation.config';
+import tailwindTypographyConfig from './tailwind_typography.config';
 
 const baseConfig: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
-const config: Config = merge(
-  baseConfig,
-  tailwindCustomConfig,
-  tailwindElevationConfig
-);
+const config: Config = merge(baseConfig, tailwindCustomConfig, tailwindElevationConfig, tailwindTypographyConfig);
 
 export default config;
