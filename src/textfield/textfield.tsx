8d8dcd505971e1d555d54e391558d201bd417cb1
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-16 20:27:23 +0900

    FEAT: TextField 컴포넌트 구현

---
+++ src/textfield/textfield.tsx
@@ -0,0 +1,221 @@
+import { Suspense, useEffect, useState } from "react";
+import ErrorIcon from "../icon/svg/status/warning-circle-filled.svg";
+import WarnIcon from "../icon/svg/status/warning-triangle-filled.svg";
+import TextFieldSkeleton from "./textfieldSkeleton";
+
+type StateType = "active" | "warning" | "error" | "disabled" | "readOnly";
+
+type TextFieldProps = {
+  size: "S" | "M" | "L";
+  style: "outlined" | "underlined";
+  state?: StateType;
+  label: boolean;
+  labelText?: string;
+  description: boolean;
+  descriptionText?: string;
+  placeholder?: string;
+};
+
+const lineStyle = {
+  outlined: {
+    inputPX: "px-4",
+  },
+  underlined: {
+    inputPX: "pr-4",
+  },
+};
+
+const sizeStyle = {
+  S: {
+    label: "text-helpertext-01-regular",
+    inputPY: "py-[11px]",
+    inputFont: "text-label-02-medium",
+    iconSize: 16,
+    description: "text-helpertext-01-regular",
+  },
+  M: {
+    label: "text-helpertext-01-regular",
+    inputPY: "py-[13px]",
+    inputFont: "text-label-03-medium",
+    iconSize: 16,
+    description: "text-helpertext-01-regular",
+  },
+  L: {
+    label: "text-helpertext-02-regular",
+    inputPY: "py-[15px]",
+    inputFont: "text-label-04-medium",
+    iconSize: 24,
+    description: "text-helpertext-02-regular",
+  },
+};
+
+const stateStyle = {
+  active: {
+    labelColor: "text-[#6F6F6F]",
+    descriptionColor: "text-[#6F6F6F]",
+    // labelColor: "text-secondary",
+    // descriptionColor: "text-helper",
+  },
+  warning: {
+    labelColor: "text-[#6F6F6F]",
+    descriptionColor: "text-[#161616]",
+    iconColor: "#F1C21B",
+    // labelColor: "text-secondary",
+    // descriptionColor: "text-primary",
+  },
+  error: {
+    labelColor: "text-[#6F6F6F]",
+    descriptionColor: "text-[#DA1E28]",
+    iconColor: "#DA1E28",
+    // labelColor: "text-secondary",
+    // descriptionColor: "text-error",
+  },
+  disabled: {
+    labelColor: "text-[#161616]/25",
+    descriptionColor: "text-[#161616]/25",
+    // labelColor: "text-disabled",
+    // descriptionColor: "text-disabled",
+  },
+  readOnly: {
+    labelColor: "text-[#6F6F6F]",
+    descriptionColor: "text-[#6F6F6F]",
+    // labelColor: "text-secondary",
+    // descriptionColor: "text-disabled",
+  },
+};
+
+export default function TextField({
+  size,
+  style = "outlined",
+  state = "active",
+  label,
+  labelText,
+  description,
+  descriptionText,
+  placeholder,
+}: TextFieldProps) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [inputBorder, setInputBorder] = useState<string>("");
+
+  const borderStyle = () => {
+    if (style === "outlined") {
+      switch (state) {
+        case "disabled":
+          setInputBorder("border border-[#8D8D8D] rounded-lg");
+          //   outlined: "border border-strong-01 rounded-lg",
+          break;
+        case "readOnly":
+          setInputBorder("border border-[#E0E0E0] rounded-lg");
+          //   outlined: "border border-tile-01 rounded-lg",
+          break;
+        case "error":
+          setInputBorder("border-2 border-[#DA1E28] rounded-lg");
+          //   outlined: "border-2 border-error rounded-lg",
+          break;
+        case "warning":
+          setInputBorder("border-2 border-[#8D8D8D] rounded-lg");
+          //   outlined: "border-2 border-strong-01 rounded-lg",
+          break;
+        default:
+          if (isFocused) setInputBorder("border-2 border-[#C6C6C6] rounded-lg");
+          else setInputBorder("border border-[#C6C6C6] rounded-lg");
+          //   outlined: "border border-subtle-01 rounded-lg",
+          break;
+      }
+    } else {
+      switch (state) {
+        case "disabled":
+          setInputBorder("border-b border-b-[#8D8D8D]");
+          //   underlined: "border-b border-b-strong-01",
+          break;
+        case "readOnly":
+          setInputBorder("border-b border-b-[#E0E0E0]");
+          //   underlined: "border-b border-b-tile-01",
+          break;
+        case "error":
+          setInputBorder("border-b-2 border-b-[#DA1E28]");
+          //   underlined: "border-b-2 border-b-error",
+          break;
+        case "warning":
+          setInputBorder("border-b-2 border-b-[#8D8D8D]");
+          //   underlined: "border-b-2 border-b-strong-01",
+          break;
+        default:
+          if (isFocused)
+            setInputBorder("border-b-2 border-b-[#C6C6C6] rounded-lg");
+          else setInputBorder("border-b border-b-[#C6C6C6]");
+          //   underlined: "border-b border-b-subtle-01",
+          break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    borderStyle();
+  }, [isFocused, style]);
+
+  return (
+    <Suspense fallback={<TextFieldSkeleton size={size} style={style} />}>
+      <div className="flex flex-col gap-2 w-full">
+        <div
+          className={`
+          ${style === "outlined" && "pl-4"}
+          ${sizeStyle[size]["label"]} 
+          ${stateStyle[state]["labelColor"]} 
+          ${!label && "hidden"}  
+        `}
+        >
+          {labelText}
+        </div>
+        <div
+          className={`
+            w-full flex gap-4 bg-[#fff] 
+            ${inputBorder}
+            ${lineStyle[style]["inputPX"]}
+            ${sizeStyle[size]["inputPY"]} 
+          `}
+        >
+          <input
+            type="text"
+            readOnly={state === "readOnly"}
+            placeholder={placeholder}
+            disabled={state === "disabled"}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            className={`
+            w-full outline-none
+            placeholder:text-[#A8A8A8] 
+            disabled:bg-[#fff] disabled:text-[#161616]/25
+            text-[#161616]
+            ${sizeStyle[size]["inputFont"]}
+          `}
+          />
+          {state === "error" && (
+            <ErrorIcon
+              width={sizeStyle[size]["iconSize"]}
+              height={sizeStyle[size]["iconSize"]}
+              color={stateStyle[state]["iconColor"]}
+            />
+          )}
+          {state === "warning" && (
+            <WarnIcon
+              width={sizeStyle[size]["iconSize"]}
+              height={sizeStyle[size]["iconSize"]}
+              color={stateStyle[state]["iconColor"]}
+            />
+          )}
+        </div>
+        <div
+          className={`
+          ${style === "outlined" && "pl-4"}
+          ${!description && "invisible"} 
+          ${sizeStyle[size]["description"]} 
+          ${stateStyle[state]["descriptionColor"]}
+      `}
+        >
+          {descriptionText}
+        </div>
+      </div>
+    </Suspense>
+  );
+}
---
+++ src/textfield/textfieldSkeleton.tsx
@@ -0,0 +1,46 @@
+type SkeletonType = {
+  size: "S" | "M" | "L";
+  style: "outlined" | "underlined";
+};
+
+const boxStyle = {
+  S: {
+    label: "w-[29px] h-[14px]",
+    input: "h-[38px]",
+    description: "w-[57px] h-[14px]",
+  },
+  M: {
+    label: "w-[29px] h-[16px]",
+    input: "h-[46px]",
+    description: "w-[57px] h-[16px]",
+  },
+  L: {
+    label: "w-[29px] h-[16px]",
+    input: "h-[58px]",
+    description: "w-[57px] h-[16px]",
+  },
+};
+
+export default function TextFieldSkeleton({ size, style }: SkeletonType) {
+  return (
+    <div className="w-full flex flex-col gap-1">
+      <div
+        className={`
+          bg-[#d9d9d9] 
+          ${style === "outlined" && "pl-4"} 
+          ${boxStyle[size]["label"]}
+        `}
+      />
+      <div
+        className={`bg-[#d9d9d9] rounded-lg w-full ${boxStyle[size]["input"]}`}
+      />
+      <div
+        className={`
+          bg-[#d9d9d9] 
+          ${style === "outlined" && "pl-4"} 
+          ${boxStyle[size]["description"]}
+        `}
+      />
+    </div>
+  );
+}
