b37a8493a0183595d734907eb1987b868badb0bd
PangKiYeon <pdrkeamy@gmail.com>
2024-07-17 21:42:42 +0900

    FEAT : Icon 색깔 변경 기능 추가

--- src/icon/Icon.tsx
+++ src/icon/Icon.tsx
@@ -63,12 +63,20 @@ type IconProps = {
 
 /**
  * 이 컴포넌트는 svg 형태로 아이콘을 보여주며, props를 사용하여 아이콘의 크기와 스타일을 정의 할 수 있습니다.
+ * 색상은 classname 에서 fill-{color token} 으로 지정해주세요.
  */
 
 const Icon = ({ icon, size = 24, className }: IconProps) => {
     const SVGIcon = icons[icon];
 
-    return <SVGIcon className={className} width={size ?? 24} height={size ?? 24} />;
+    return (
+        <SVGIcon
+            className={`${className} ${className?.includes('fill-') ? '' : 'fill-black'}`}
+            width={size ?? 24}
+            height={size ?? 24}
+            fill="currentColor"
+        />
+    );
 };
 
 export default Icon;
