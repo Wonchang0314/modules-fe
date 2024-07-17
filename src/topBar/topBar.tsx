6afab355858245d0086dd97f4fe2f59bd193791b
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-17 14:31:05 +0900

    FEAT: Top Bar 각 버튼에 onClick 함수, Unread 표시 추가

--- src/topBar/topBar.tsx
+++ src/topBar/topBar.tsx
@@ -15,6 +15,18 @@ type TopBarProps = {
    * type: string | iconKey
    */
   rightText?: string | iconKey;
+  onClickLeftIcon?: () => void;
+  onClickLeftText?: () => void;
+  onClickRightIcon?: () => void;
+  onClickRightText?: () => void;
+  /**
+   * true일 때 안 읽음 표시
+   */
+  rightIconUnread?: boolean;
+  /**
+   * rightText=iconKey일 때만 표시
+   */
+  rightTextUnread?: boolean;
 };
 export default function TopBar({
   title,
@@ -22,6 +34,12 @@ export default function TopBar({
   leftText,
   rightIcon,
   rightText,
+  onClickLeftIcon,
+  onClickLeftText,
+  onClickRightIcon,
+  onClickRightText,
+  rightIconUnread = false,
+  rightTextUnread = false,
 }: TopBarProps) {
   let SVGLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> | undefined;
   let SVGRightIcon: React.FC<React.SVGProps<SVGSVGElement>> | undefined;
@@ -38,10 +56,21 @@ export default function TopBar({
     else if (isIconKey(rightText)) {
       const SVGRightIcon2: React.FC<React.SVGProps<SVGSVGElement>> =
         icons[rightText as iconKey];
-      return <SVGRightIcon2 width={20} height={20} />;
+      return (
+        <div onClick={onClickRightText} className="relative">
+          <SVGRightIcon2 width={20} height={20} />
+          {rightTextUnread && (
+            <div className="absolute top-0 right-0 w-2 h-2 bg-[#DA1E28] rounded-full" />
+            /* <div className="absolute top-0 right-0 w-2 h-2 bg-support-error rounded-full" /> */
+          )}
+        </div>
+      );
     } else
       return (
-        <div className="text-[#0043CE] text-label-02-medium">
+        <div
+          className="text-[#0043CE] text-label-02-medium"
+          onClick={onClickRightText}
+        >
           {/* <div className="text-support-info text-label-02-medium"> */}
           {rightText}
         </div>
@@ -51,9 +80,18 @@ export default function TopBar({
   return (
     <div className="flex w-full h-[32px] bg-[#FFF] justify-between items-center px-4 py-2">
       <div className="flex items-center gap-5">
-        {SVGLeftIcon && <SVGLeftIcon width={20} height={20} />}
-        {typeof leftText === "string" && (
-          <div className="text-label-02-meidum text-[#161616]">{leftText}</div>
+        {SVGLeftIcon && (
+          <div onClick={onClickLeftIcon}>
+            <SVGLeftIcon width={20} height={20} />
+          </div>
+        )}
+        {leftText && (
+          <div
+            className="text-label-02-meidum text-[#161616]"
+            onClick={onClickLeftText}
+          >
+            {leftText}
+          </div>
           // <div className="text-label-02-meidum text-primary">{leftText}</div>
         )}
       </div>
@@ -61,7 +99,13 @@ export default function TopBar({
       {/* <div className="text-labe-03-medium text-primary">{title}</div> */}
       <div className="flex justify-end items-center gap-5">
         {returnRightText()}
-        {SVGRightIcon && <SVGRightIcon width={20} height={20} />}
+        {SVGRightIcon && (
+          <div onClick={onClickRightIcon} className="relative">
+            <SVGRightIcon width={20} height={20} />
+            <div className="absolute top-0 right-0 w-2 h-2 bg-[#DA1E28] rounded-full" />
+            {/* <div className="absolute top-0 right-0 w-2 h-2 bg-support-error rounded-full" /> */}
+          </div>
+        )}
       </div>
     </div>
   );
