26b5c408f56d83677a96fb4e3b2ce82f6392b510
PangKiYeon <pdrkeamy@gmail.com>
2024-07-19 03:08:00 +0900

    FIX : Accordion padding 수정

--- src/accordion/Accordion.stories.tsx
+++ src/accordion/Accordion.stories.tsx
@@ -1,35 +1,37 @@
-import { Meta, StoryFn } from '@storybook/react/*';
-import Accordion, { AccordionProps } from './Accordion';
+import { Meta, StoryFn } from "@storybook/react/*";
+import Accordion, { AccordionProps } from "./Accordion";
 
 export default {
-    title: 'Layout/Accordion',
-    component: Accordion,
-    parameters: {
-        layout: 'centered',
-        backgrounds: { default: 'light' },
-    },
-    tags: ['autodocs'],
+  title: "Layout/Accordion",
+  component: Accordion,
+  parameters: {
+    layout: "centered",
+    backgrounds: { default: "light" },
+  },
+  tags: ["autodocs"],
 } as Meta<typeof Accordion>;
 
-const Template: StoryFn<AccordionProps> = (args) => <Accordion {...args} />;
+const Template: StoryFn<AccordionProps> = args => <Accordion {...args} />;
 
 export const CustomAccordion = Template.bind({});
 CustomAccordion.args = {
-    state: 'Disabled',
-    size: 'L',
-    hover: true,
-    focus: false,
-    data: [
-        {
-            title: 'Title',
-            content:
-                'An accordion is a vertically stacked list of headers that reveal or hide associated sections of content.',
-            children: <div className="w-[336px] h-[32px] bg-black mt-3 text-white p-2">changer</div>,
-        },
-        {
-            title: 'Title',
-            content:
-                'An accordion is a vertically stacked list of headers that reveal or hide associated sections of content.',
-        },
-    ],
+  state: "Disabled",
+  size: "L",
+  data: [
+    {
+      title: "Title",
+      content:
+        "An accordion is a vertically stacked list of headers that reveal or hide associated sections of content.",
+      children: (
+        <div className="w-[336px] h-[32px] bg-black text-white p-2">
+          changer
+        </div>
+      ),
+    },
+    {
+      title: "Title",
+      content:
+        "An accordion is a vertically stacked list of headers that reveal or hide associated sections of content.",
+    },
+  ],
 };
