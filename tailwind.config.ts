5da71962b8bad77b8f07b992578a0a81fb7cb841
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-07-18 19:44:39 +0900

    Revert "Merge branch 'StorybookNpm' into feat-textarea"
    This reverts commit 5d3ae842f51820d63d08d8e2e19d01cb85ef9000, reversing
    changes made to cff87d567ea1a9bfdc9c364bbac74c07329dff02.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss';
 import { merge } from 'lodash';
 import ColorGroundConfig from './colorGround/ColorGround.config';
-import tailwindColorConfig from './themeGround/tailwind_color.config';
-import tailwindElevationConfig from './themeGround/tailwind_elevation.config';
-import tailwindSpacingConfig from './themeGround/tailwind_spacing.config';
-import tailwindRadiusConfig from './themeGround/tailwind_radius.config';
-import tailwindMotionsConfig from './themeGround/tailwind_motions.config';
-import tailwindTypographyConfig from './themeGround/tailwind_typography.config';
+import tailwindColorConfig from './themeGround/foundation/tailwind_color.config';
+import tailwindElevationConfig from './themeGround/foundation/tailwind_elevation.config';
+import tailwindSpacingConfig from './themeGround/foundation/tailwind_spacing.config';
+import tailwindRadiusConfig from './themeGround/foundation/tailwind_radius.config';
+import tailwindMotionsConfig from './themeGround/foundation/tailwind_motions.config';
+import tailwindTypographyConfig from './themeGround/foundation/tailwind_typography.config';
 
 const baseConfig: Config = {
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
