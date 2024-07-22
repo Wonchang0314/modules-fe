848bc7c929bd4827292db25ef1c372c2256b6916
developerryou <developerryou@gmail.com>
2024-07-22 15:36:36 +0900

    FIX: backgroundColor 추가

--- src/display/avatar/Avatar.tsx
+++ src/display/avatar/Avatar.tsx
@@ -3,7 +3,21 @@ import Icon, { iconKey } from "src/icon/Icon";
 export interface AvatarProps {
   input?: "text" | "icon" | "image";
   size: "S" | "M" | "L" | "XL";
-  backgroundColor?: string;
+  backgroundColor?:
+    | "in-gray"
+    | "in-red"
+    | "in-green"
+    | "in-blue"
+    | "in-cyan"
+    | "in-magenta"
+    | "in-purple"
+    | "in-teal"
+    | "out-red"
+    | "out-red-2"
+    | "out-orange"
+    | "out-blue"
+    | "out-cyan"
+    | "out-purple";
   border?: boolean;
   text?: string;
   icon?: iconKey;
@@ -38,9 +52,40 @@ const borderStyle = {
   XL: "border border-4 border-[#262626]",
 };
 
+const bgStyle = {
+  "in-gray": "from-[#C6C6C6] to-[#393939]",
+  //background: linear-gradient(180deg, #FF8389 0%, #A2191F 100%);
+  "in-red": "from-[#FF8389] to-[#A2191F]",
+  //background: linear-gradient(180deg, #6FDC8C 0%, #0E6027 100%);
+  "in-green": "from-[#6FDC8C] to-[#0E6027]",
+  //background: linear-gradient(180deg, #A6C8FF 0%, #0043CE 100%);
+  "in-blue": "from-[#A6C8FF] to-[#0043CE]",
+  //background: linear-gradient(180deg, #82CFFF 0%, #00539A 100%);
+  "in-cyan": "from-[#82CFFF] to-[#00539A]",
+  //  background: linear-gradient(180deg, #FFAFD2 0%, #D02670 100%);
+  "in-magenta": "from-[#FFAFD2] to-[#D02670]",
+  //background: linear-gradient(180deg, #D4BBFF 0%, #6929C4 100%);
+  "in-purple": "from-[#D4BBFF] to-[#6929C4]",
+  //background: linear-gradient(180deg, #3DDBD9 0%, #005D5D 100%);
+  "in-teal": "from-[#3DDBD9] to-[#005D5D]",
+  //background: linear-gradient(180deg, #FFB784 0%, #DA1E28 100%);
+  "out-red": "from-[#FFB784] to-[#DA1E28]",
+  //background: linear-gradient(180deg, #F1C21B 0%, #DA1E28 100%);
+  "out-red-2": "from-[#F1C21B] to-[#DA1E28]",
+  //background: linear-gradient(180deg, #FDDC69 0%, #EB6200 100%);
+  "out-orange": "from-[#FDDC69] to-[#EB6200]",
+  //background: linear-gradient(180deg, #82CFFF 0%, #0043CE 100%);
+  "out-blue": "from-[#82CFFF] to-[#0043CE]",
+  //background: linear-gradient(180deg, #3DDBD9 0%, #00539A 100%);
+  "out-cyan": "from-[#3DDBD9] to-[#00539A]",
+  //background: linear-gradient(180deg, #FFAFD2 0%, #8A3FFC 100%);
+  "out-purple": "from-[#FFAFD2] to-[#8A3FFC]",
+};
+
 export default function Avatar({
   input = "text",
   size = "M",
+  backgroundColor = "in-gray",
   border = false,
   text = "",
   icon = "account",
@@ -50,8 +95,9 @@ export default function Avatar({
     <div
       className={`flex justify-center items-center rounded-full relative ${
         border ? borderStyle[size] : ""
-      } ${sizeStyle[size]} 
-      bg-gradient-to-b from-[#C6C6C6] to-[#393939]
+      } ${sizeStyle[size]} bg-gradient-to-b ${
+        input !== "image" ? bgStyle[backgroundColor] : ""
+      }
       `}
     >
       {input === "text" && <div className={fontStyle[size]}>{text?.[0]}</div>}
