9da05dfef12c0c3911bda946f8d13f306f04edf0
PangKiYeon <pdrkeamy@gmail.com>
2024-08-07 21:45:49 +0900

    FIX :  Divider subheader 의존성 수정

--- src/divider/Divider.stories.tsx
+++ src/divider/Divider.stories.tsx
@@ -1,26 +1,27 @@
-import { Meta, StoryFn } from '@storybook/react/*';
-import Divider, { DividerProps } from './Divider';
+import { Meta, StoryFn } from "@storybook/react/*";
+import Divider, { DividerProps } from "./Divider";
 
 export default {
-    title: 'Layout/Divider',
-    component: Divider,
-    parameters: {
-        layout: 'centered',
-        backgrounds: { default: 'light' },
-    },
+  title: "Layout/Divider",
+  component: Divider,
+  parameters: {
+    layout: "centered",
+    backgrounds: { default: "light" },
+  },
 } as Meta<typeof Divider>;
 
-const Template: StoryFn<DividerProps> = (args) => <Divider {...args} />;
+const Template: StoryFn<DividerProps> = args => <Divider {...args} />;
 
 export const Horizontal = Template.bind({});
 Horizontal.args = {
-    type: 'Horizontal',
-    height: 1,
-    subheader: " I'm a subheader",
+  type: "Horizontal",
+  size: 100,
+  subheader: " I'm a subheader",
 };
 
 export const Vertical = Template.bind({});
 Vertical.args = {
-    type: 'Vertical',
-    height: 100,
+  type: "Vertical",
+  size: 100,
+  className: "bg-support-error",
 };
