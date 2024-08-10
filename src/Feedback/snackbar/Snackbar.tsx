ae21dfacb1b8174398fea7b87d498838c006f656
PangKiYeon <pdrkeamy@gmail.com>
2024-08-10 22:36:16 +0900

    FEAT : Snackbar motion 추가

--- src/Feedback/snackbar/Snackbar.tsx
+++ src/Feedback/snackbar/Snackbar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Icon from "src/icon/Icon";
 
 export interface SnackbarProps {
@@ -11,8 +12,6 @@ export interface SnackbarProps {
   onClose?: () => void;
 }
 
-// TODO : 애니메이션 정의 (Motion 사용)
-
 export default function Snackbar({
   open,
   message,
@@ -23,49 +22,87 @@ export default function Snackbar({
   actionOnClick,
   onClose,
 }: SnackbarProps) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [shouldRender, setShouldRender] = useState(open);
+
+  useEffect(() => {
+    let showTimeout: NodeJS.Timeout;
+    let hideTimeout: NodeJS.Timeout;
+
+    if (open) {
+      setShouldRender(true);
+      showTimeout = setTimeout(() => {
+        setIsVisible(true);
+      }, 10);
+
+      hideTimeout = setTimeout(() => {
+        setIsVisible(false);
+        if (onClose) {
+          onClose(); // onClose에서 open을 false로 변경
+        }
+      }, 1500);
+    } else {
+      setIsVisible(false);
+      hideTimeout = setTimeout(() => {
+        setShouldRender(false);
+      }, 300);
+    }
+
+    return () => {
+      clearTimeout(showTimeout);
+      clearTimeout(hideTimeout);
+    };
+  }, [open]);
+
+  if (!shouldRender) return null;
+
   return (
-    open && (
-      <div
-        className={`flex flex-row px-spacing-05 py-spacing-03 bg-layer-inverse rounded-full w-full
+    <div
+      className={`flex flex-row px-spacing-05 py-spacing-03 bg-layer-inverse rounded-full w-full
+          transition-opacity
+          ${
+            isVisible
+              ? "opacity-100 duration-duration-07 ease-tranquillo-default"
+              : "opacity-0 duration-duration-05 ease-tranquillo-exit"
+          }
           ${!alert && !action && !dismissible ? "justify-center text-center" : "justify-between"}`}
-      >
-        <div className="flex flex-row items-center">
-          {alert && (
-            <Icon
-              icon={"warning_triangle_filled"}
-              className="fill-support-warning mr-spacing-02"
-              size={20}
-            />
+    >
+      <div className="flex flex-row items-center">
+        {alert && (
+          <Icon
+            icon={"warning_triangle_filled"}
+            className="fill-support-warning mr-spacing-02"
+            size={20}
+          />
+        )}
+        <span className="text-text-on-color body-01-medium">
+          {message}
+          {lineMessage && (
+            <>
+              <br />
+              {lineMessage}
+            </>
           )}
-          <span className="text-text-on-color body-01-medium">
-            {message}
-            {lineMessage && (
-              <>
-                <br />
-                {lineMessage}
-              </>
-            )}
-          </span>
-        </div>
-        <div className="flex flex-row items-center">
-          {action && (
-            <button
-              className="text-support-info-inverse body-01-bold-compact"
-              onClick={actionOnClick}
-            >
-              {action}
-            </button>
-          )}
-          {dismissible && (
-            <button
-              onClick={onClose}
-              className={`flex ${action ? "ml-spacing-02" : "ml-spacing-04"}`}
-            >
-              <Icon icon={"close"} size={20} className="fill-icon-on-color" />
-            </button>
-          )}
-        </div>
+        </span>
       </div>
-    )
+      <div className="flex flex-row items-center">
+        {action && (
+          <button
+            className="text-support-info-inverse body-01-bold-compact"
+            onClick={actionOnClick}
+          >
+            {action}
+          </button>
+        )}
+        {dismissible && (
+          <button
+            onClick={onClose}
+            className={`flex ${action ? "ml-spacing-02" : "ml-spacing-04"}`}
+          >
+            <Icon icon={"close"} size={20} className="fill-icon-on-color" />
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
