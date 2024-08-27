7279f3ba239ca9a05940049a28a7822f6a246a38
Wonchang0314 <andrew0314@naver.com>
2024-08-27 15:30:55 +0900

    FIX: 복구

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,3 @@ export * from "./Feedback";
 export * from "./Layout";
 export * from "./Display";
 export * from "./Input";
-// 스타일관련 설정 파일들
-export { default as ColorGroundConfig } from "../tailwindConfig/colorGround/ColorGround.config";
-export { default as tailwindColorConfig } from "../tailwindConfig/themeGround/tailwind_color.config";
-export { default as tailwindElevationConfig } from "../tailwindConfig/themeGround/tailwind_elevation.config";
-export { default as tailwindSpacingConfig } from "../tailwindConfig/themeGround/tailwind_spacing.config";
-export { default as tailwindRadiusConfig } from "../tailwindConfig/themeGround/tailwind_radius.config";
-export { default as tailwindMotionsConfig } from "../tailwindConfig/themeGround/tailwind_motions.config";
-export { default as tailwindTypographyConfig } from "../tailwindConfig/themeGround/tailwind_typography.config";
