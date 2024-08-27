b22672ec0335993dba6e6ff4044b003dae044206
Wonchang0314 <andrew0314@naver.com>
2024-08-27 15:54:12 +0900

    FIX: tailwind 설정 파일 내보내기 추가

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ export * from "./Feedback";
 export * from "./Layout";
 export * from "./Display";
 export * from "./Input";
+
+// Tailwind 관련 설정들
+export { default as ColorGroundConfig } from "../tailwindConfig/colorGround/ColorGround.config";
+export { default as tailwindColorConfig } from "../tailwindConfig/themeGround/tailwind_color.config";
+export { default as tailwindElevationConfig } from "../tailwindConfig/themeGround/tailwind_elevation.config";
+export { default as tailwindSpacingConfig } from "../tailwindConfig/themeGround/tailwind_spacing.config";
+export { default as tailwindRadiusConfig } from "../tailwindConfig/themeGround/tailwind_radius.config";
+export { default as tailwindMotionsConfig } from "../tailwindConfig/themeGround/tailwind_motions.config";
+export { default as tailwindTypographyConfig } from "../tailwindConfig/themeGround/tailwind_typography.config";
