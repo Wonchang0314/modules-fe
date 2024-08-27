d715b21ea65a42325bea5b64a3d242a3f05742b8
Wonchang0314 <andrew0314@naver.com>
2024-08-27 16:18:38 +0900

    FEAT: 커스텀 Tailwind 설정 파일을 패키지에 포함

---
+++ custom-tailwind-config.ts
@@ -0,0 +1,8 @@
+// 사용할 디자인 설정 파일
+export { default as ColorGroundConfig } from "./tailwindConfig/colorGround/ColorGround.config";
+export { default as tailwindColorConfig } from "./tailwindConfig/themeGround/tailwind_color.config";
+export { default as tailwindElevationConfig } from "./tailwindConfig/themeGround/tailwind_elevation.config";
+export { default as tailwindSpacingConfig } from "./tailwindConfig/themeGround/tailwind_spacing.config";
+export { default as tailwindRadiusConfig } from "./tailwindConfig/themeGround/tailwind_radius.config";
+export { default as tailwindMotionsConfig } from "./tailwindConfig/themeGround/tailwind_motions.config";
+export { default as tailwindTypographyConfig } from "./tailwindConfig/themeGround/tailwind_typography.config";
