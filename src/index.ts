78c541b121887e0c8836159fafbe3ccc93dcdde2
Wonchang0314 <andrew0314@naver.com>
2024-10-07 18:21:08 +0900

    FIX: 빌드 결과물만 NPM에 포함하도록 수정

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import "./styles/index";
-export * from "./styles";
 export * from "./tailwindConfig";
+export * from "./icon";
+export * from "./Navigation";
+export * from "./Feedback";
+export * from "./Layout";
+export * from "./Display";
+export * from "./Input";
